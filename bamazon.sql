eef1901861d2b529347cea02a488dd53c4017f11
Jorge Mancero <jman0829@gmail.com>
2019-11-24 16:59:35 -0500

    updates

--- bamazon.sql
+++ bamazon.sql
@@ -1,24 +1,24 @@
-drop database if exists bamazon_db;
-create database bamazon_db;
-use bamazon_db;
+-- drop database if exists bamazon_db;
+-- create database bamazon_db;
+-- use bamazon_db;
 
-create table products (
-item_id integer not null auto_increment,
-product_name varchar (70) not null,
-department_name varchar (70) not null,
-price decimal (10,2) not null,
-stock_quantity integer not null,
-primary key (item_id)
-);
+-- create table products (
+-- item_id integer not null auto_increment,
+-- product_name varchar (70) not null,
+-- department_name varchar (70) not null,
+-- price decimal (10,2) not null,
+-- stock_quantity integer not null,
+-- primary key (item_id)
+-- );
 
-insert into products (product_name, department_name, price, stock_quantity)
-values ("iphone", "electronics", 700.00, 10),
-	   ("laptop", "electronics", 1000.00, 5),
-       ("shirts", "clothing", 30.00, 20),
-       ("pants", "clothing", 50.00, 30),
-       ("ipad", "electronics", 900.00, 10),
-       ("shoes", "clothing", 100.00, 25),
-       ("sofa", "furniture", 500.00, 5),
-       ("socks", "clothing", 10.00, 40),
-       ("love seat", "furtniture", 600.00, 12),
-       ("table", "furniture", 100.00, 30);
+-- insert into products (product_name, department_name, price, stock_quantity)
+-- values ("iphone", "electronics", 700.00, 10),
+-- 	   ("laptop", "electronics", 1000.00, 5),
+--        ("shirts", "clothing", 30.00, 20),
+--        ("pants", "clothing", 50.00, 30),
+--        ("ipad", "electronics", 900.00, 10),
+--        ("shoes", "clothing", 100.00, 25),
+--        ("sofa", "furniture", 500.00, 5),
+--        ("socks", "clothing", 10.00, 40),
+--        ("love seat", "furtniture", 600.00, 12),
+--        ("table", "furniture", 100.00, 30);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,39 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+    .prompt({
+        name:"seeItems",
+        type:"list",
+        message: "Would you like to buy one of our items?",
+        choices: ["YES", "NO"]
+    })
+
+    .then(function(answer) {
+        innerHeight(answer.seeItems === "YES") {
+            displayItems();
+        }
+        else if(answer.seeItems === "NO") {
+            
+        }
+        else{
+        connection.end();
+        }
+    });
+}
+
+function displayItems() {
+    console.log("Items up for sale");
+  console.log("------------------");
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | Unit Price: $" + res[i].price + " | Units Remaining: " + res[i].quantity);
+      console.log("------------------");
+    }
+}
